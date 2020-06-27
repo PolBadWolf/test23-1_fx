993c5e2e6784b0021a135c9d162d4ba08789d359
PolBadWolf <gm.gameoff@gmail.com>
2020-06-27 14:26:21 +0500

    Initial commit

---
+++ src/sample/Controller_Sample.java
@@ -0,0 +1,19 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller_Sample implements Initializable {
+
+    @FXML
+    public Label label1;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,57 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static Controller_Sample controllerSample = null;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
+        Parent root = (Parent) loader.load();
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root));
+        controllerSample = loader.getController();
+        primaryStage.show();
+        (new Thread(new MyNit())).start();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private static class MyNit implements Runnable {
+        private Thread thread = null;
+
+        public void start(Thread thread) {
+            this.thread = thread;
+            this.thread.start();
+        }
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(10_000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Platform.runLater(()->
+                    internalFn1());
+            try {
+                Thread.sleep(3_000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.exit(0);
+        }
+
+        private void internalFn1() {
+            controllerSample.label1.setText("wait 3 second and app off");
+        }
+    }
+}
